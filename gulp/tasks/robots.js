11250c43b09c4cc08f5bde94c5d97a7fffca65a3
Zell Liew <zellwk@gmail.com>
2020-03-11 10:26:42 +0800

    Change generator

--- gulp/tasks/robots.js
+++
@@ -1,6 +0,0 @@
-const gulp = require('gulp')
-
-gulp.task('robots', () => {
-  gulp.src('src/robots.txt')
-    .pipe(gulp.dest('dist'))
-})
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,32 @@
-const requireDir = require('require-dir')
+require('dotenv').config({ path: 'secrets/variables.env' })
+const { series, parallel } = require('gulp')
+const clean = require('./gulp/clean')
+const eleventy = require('./gulp/eleventy')
+const sass = require('./gulp/sass')
+const { jsDevelopment, jsProduction } = require('./gulp/rollup')
+const images = require('./gulp/images')
+const watch = require('./gulp/watch')
+const { browserSync } = require('./gulp/browser-sync')
+const rev = require('./gulp/rev')
 
-// Require gulp from subdirectories
-requireDir('./gulp/tasks')
+exports.clean = clean
+exports.eleventy = eleventy
+exports.sass = sass
+exports.jsdev = jsDevelopment
+exports.jsprod = jsProduction
+exports.images = images
+exports.serve = browserSync
+exports.rev = rev
+
+exports.default = series(
+  clean,
+  parallel(sass, eleventy, images),
+  parallel(jsDevelopment, browserSync, watch)
+)
+
+exports.build = series(
+  clean,
+  parallel(sass, images, jsProduction),
+  rev,
+  eleventy
+)
